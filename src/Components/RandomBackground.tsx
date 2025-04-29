612409a41852d0dffcbb9295bca26ebd088b3816
czlabinger <czlabinger@student.tgm.ac.at>
2025-04-29 22:48:39 +0200

    Images must be working now

--- src/Components/RandomBackground.tsx
+++ src/Components/RandomBackground.tsx
@@ -58,7 +58,7 @@ const RandomBackgroundImages: React.FC<RandomBackgroundImagesProps> = ({
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      setUrl(window.location.origin);
+      setUrl(window.location.href + window.location.pathname);
     }
   }, []);
 
